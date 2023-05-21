62ad397bf92dbec84a5a2b38be735f39a21a6868
Pattriarch <pgelo298@mail.ru>
2023-05-22 00:12:11 +0300

    Урок 7. Добавление тем, организация стилей.

---
+++ src/theme/useTheme.ts
@@ -0,0 +1,24 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {useContext} from "react";
+
+interface UseThemeResult {
+	toggleTheme: () => void;
+	theme: Theme | undefined;
+}
+
+export function useTheme(): UseThemeResult {
+	const {theme, setTheme} = useContext(ThemeContext);
+
+	const toggleTheme = () => {
+		if (setTheme) {
+			const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+			setTheme(newTheme);
+			localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+		}
+	}
+
+	return {
+		toggleTheme,
+		theme
+	}
+}
