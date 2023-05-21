8e7c7904a8d7f5058e66ed0bc7137227ab9cf0af
Pattriarch <pgelo298@mail.ru>
2023-05-21 23:31:29 +0300

    Урок 6. Code-Splitting, Lazy, Suspense.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import {render} from "react-dom";
 import Counter from "./components/Counter";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
 render(
-    <App/>,
+	<BrowserRouter>
+		<App/>
+	</BrowserRouter>,
     document.getElementById('root')
 )
