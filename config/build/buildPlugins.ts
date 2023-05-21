8411635f0174dc94d878811b80c5c3310eaf33e3
Pattriarch <pgelo298@mail.ru>
2023-05-21 17:53:49 +0300

    Урок 5. Добавление css модулей.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,5 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 
@@ -7,6 +8,10 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         new HTMLWebpackPlugin({
             template: paths.html,
         }),
-        new webpack.ProgressPlugin()
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
     ]
 }
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from "./components/Counter";
+import './index.scss';
+
+const App = () => {
+	return (
+		<div className={"app"}>
+			test text
+			<Counter/>
+		</div>
+	);
+};
+
+export default App;
