4943d4bba3ae83f76baa90cf48c54ab14fc09d06
Pattriarch <pgelo298@mail.ru>
2023-06-22 21:41:36 +0300

    Урок 16. Webpack hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({isDev, paths}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -15,6 +16,8 @@ export function buildPlugins({isDev, paths}: BuildOptions): webpack.WebpackPlugi
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new ReactRefreshPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
