18cebd53fb2cdea601f54db77198b1773484d00c
daniilpereverzev <pgelo298@mail.ru>
2023-06-23 14:06:52 +0300

    19 урок. Настройки stylelint, плагин для i18next, фикс scss-файлов

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
         browser: true, es2021: true
     },
-    extends: ['standard-with-typescript', 'plugin:react/recommended'],
+    extends: ['standard-with-typescript', 'plugin:react/recommended', 'plugin:i18next/recommended'],
     overrides: [{
         env: {
             node: true
@@ -15,7 +15,7 @@ module.exports = {
     parserOptions: {
         project: './tsconfig.json', ecmaVersion: 'latest', sourceType: 'module'
     },
-    plugins: ['react'],
+    plugins: ['react', 'i18next'],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -32,7 +32,8 @@ module.exports = {
         '@typescript-eslint/strict-boolean-expressions': 'off',
         'react/react-in-jsx-scope': 'off',
         '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/ban-ts-comment': 1
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['to'] }]
     },
     globals: {
         __IS_DEV__: true
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { useTranslation } from 'react-i18next'
 
 interface NavbarProps {
     className?: string
 }
 
 export const Navbar = ({ className }: NavbarProps): JSX.Element => {
+    const { t } = useTranslation()
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Главная</AppLink>
-                <AppLink theme={AppLinkTheme.RED} to={'/about'}>О сайте</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.RED} to={'/about'}>{t('О сайте')}</AppLink>
             </div>
         </div>
     )
