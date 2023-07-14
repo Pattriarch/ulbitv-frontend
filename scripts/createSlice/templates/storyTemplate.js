9af84049c6939751af78305b7714a48af9732a0d
daniilpereverzev <pgelo298@mail.ru>
2023-07-14 12:29:19 +0300

    69 урок. Генератор entities/features/pages в виде скрипта на node js

---
+++ scripts/createSlice/templates/storyTemplate.js
@@ -0,0 +1,16 @@
+module.exports = (layer, componentName) => `import type { Meta, StoryObj } from '@storybook/react';
+import { ${componentName} } from './${componentName}';
+
+const meta: Meta<typeof ${componentName}> = {
+    title: '${layer}/${componentName}',
+    component: ${componentName},
+    tags: ['autodocs']
+};
+
+export default meta;
+type Story = StoryObj<typeof ${componentName}>;
+
+export const Normal: Story = {
+    args: {}
+};
+`;
