39163987de97b7171d2d741e02a0b5eb00e14c80
Pattriarch <pgelo298@mail.ru>
2023-07-22 15:58:46 +0300

    96 урок. Делаем автофикс для public api линтера

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -4,18 +4,33 @@ import { useCallback } from 'react';
 import { ListBox } from '@/shared/ui/Popups';
 
 interface CountrySelectProps {
-	className?: string;
-	value?: Country;
-	onChange?: (value: Country) => void;
-	readonly?: boolean;
+    className?: string;
+    value?: Country;
+    onChange?: (value: Country) => void;
+    readonly?: boolean;
 }
 
 const options = [
-    { value: Country.Russia, content: Country.Russia },
-    { value: Country.Belarus, content: Country.Belarus },
-    { value: Country.Ukraine, content: Country.Ukraine },
-    { value: Country.Kazakhstan, content: Country.Kazakhstan },
-    { value: Country.Armenia, content: Country.Armenia }
+    {
+        value: Country.Russia,
+        content: Country.Russia
+    },
+    {
+        value: Country.Belarus,
+        content: Country.Belarus
+    },
+    {
+        value: Country.Ukraine,
+        content: Country.Ukraine
+    },
+    {
+        value: Country.Kazakhstan,
+        content: Country.Kazakhstan
+    },
+    {
+        value: Country.Armenia,
+        content: Country.Armenia
+    }
 ];
 
 export const CountrySelect = (props: CountrySelectProps): JSX.Element => {
@@ -45,15 +60,4 @@ export const CountrySelect = (props: CountrySelectProps): JSX.Element => {
             direction={'topRight'}
         />
     );
-
-    // return (
-    //     <Select
-    //         className={classNames('', {}, [className])}
-    //         label={t('Укажите страну')}
-    //         options={options}
-    //         value={value}
-    //         onChange={onChangeHandler}
-    //         readonly={readonly}
-    //     />
-    // );
 };
